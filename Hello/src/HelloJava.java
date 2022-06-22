97b27f888e238a0250e4af726fecfa9c50242cdd
wonghye <dearhw117@naver.com>
2022-06-22 09:58:52 +0900

    hello-0621

---
+++ Hello/src/HelloJava.java
@@ -0,0 +1,13 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// 출력문 .println = 줄바꿈
+		System.out.println("Hello~ Java");
+		
+		System.out.print("안녕 ~ 자바! ");
+		
+		
+	}
+
+}
