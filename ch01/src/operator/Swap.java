0b47a9c99b844ecd1b5a104005e7fd5ab7ee8a57
wonghye <dearhw117@naver.com>
2022-06-22 13:56:59 +0900

    연산자-0622

---
+++ ch01/src/operator/Swap.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// 변수의 값 교환
+		int x = 0 , y = 1;
+		int temp;   // = 0; 
+		
+		System.out.println("교환 전" );
+		System.out.println("x = " + x + ", y = "+ y);
+		System.out.println("-----------");
+		
+		//교환 처리
+		temp = x; // temp = 0
+		x = y;    // x = 1
+		y = temp; // y = 0
+		
+		System.out.println("교환 후");
+		System.out.println("x = " + x + ", y = "+ y);
+		
+	}
+
+}
