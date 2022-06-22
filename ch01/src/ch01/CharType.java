d426cae95e6d4b65a89bafc053c6f177f996523e
wonghye <dearhw117@naver.com>
2022-06-22 09:45:49 +0900

    ch01-0621

---
+++ ch01/src/ch01/CharType.java
@@ -0,0 +1,30 @@
+package ch01;
+
+public class CharType {
+
+	public static void main(String[] args) {
+		// 문자 자료형 - 아스키 코드 < 유니코드 
+		// char - 2바이트
+		char ch1 = 'A'; // 문자형 변수 ch1에 'A'를 저장
+		System.out.println(ch1);
+		System.out.println((int)ch1);  // 숫자 코드값 = 65
+		
+		char ch2 = 66;
+		System.out.println(ch2);
+		
+		System.out.println("***** 유니코드 *****");
+		
+		char uniCode1 = '한'; // 한문자이므로 홑따음표 사용
+		System.out.println(uniCode1);
+		
+		char uniCode2 = '\uD55C'; // 유니코드 한문자 표기
+		System.out.println(uniCode2);
+		
+		//영어 소문자 출력 : a ~ z 까지 출력
+		for(char c = 97; c < 123; c++) {
+			System.out.println( c + " ");
+		}
+
+	}
+
+}
---
+++ ch01/src/ch01/PrintData.java
@@ -0,0 +1,24 @@
+package ch01;
+
+public class PrintData {
+
+	public static void main(String[] args) {
+		// 숫자
+		System.out.println(100);
+		System.out.println(3.3);
+		System.out.println(100 + 10);  // 숫자 더하기
+		System.out.println("==============");
+		
+		//문자
+		System.out.println('A'); //홑따음표 - 문자 표기
+		System.out.println("B"); //홑따음표 - 문자열표기
+		System.out.println("banana"); // 쌍따음표 - 문자열표기
+		System.out.println(100 + "10"); // 쌍따음표 - 문자열표기
+		System.out.println("==============");
+		
+		//불리언 (boolean)
+		System.out.println(true);
+		System.out.println(4 > 5);
+	}
+
+}
